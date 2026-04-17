95cd555a6e39b7826345b1f40703b6c4b0343f33
chamathLONGWApps <chamath@longwapps.com>
2026-04-17 19:08:16 +0530

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+cd /var/www/buseka.lk
+
+git fetch origin
+git reset --hard origin/main
+
+rm -rf node_modules dist
+
+yarn install --frozen-lockfile
+chmod +x node_modules/.bin/vite
+yarn build
+
+find /var/www/buseka.lk/dist -type d -exec chmod 755 {} \;
+find /var/www/buseka.lk/dist -type f -exec chmod 644 {} \;
